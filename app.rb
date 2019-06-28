de68ad729f71f794ecf8b7ceb38422480bb26fc1
Andy Feidje Joseph <ingandy75@gmail.com>
2019-06-28 18:36:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    erb :reversename
+  end
+
+get '/square/:number' do
+  erb :square
+end
+
+get '/say/:number/:phrase' do
+  erb :multiply_string
+end
 
 end
